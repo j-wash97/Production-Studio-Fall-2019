bbf33898f29b418db10aa5a79e93b311a1eccd17
Julian Washingtion <cybermonkey1997@yahoo.com>
2019-09-12 13:50:07 -0400

    Map Scene Created: -MapNode.cs created -Map Tree object created in Map scene, with 5 nodes attached

---
+++ Mandate of Heaven/Assets/Scripts/MapNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    // The name of the node's location
+    public string name;
+
+    // A unique numerical ID for the node's location
+    public int nodeID;
+
+    // A collection of nodes adjacent to this one
+    public List<MapNode> adjacents;
+
+    // The node position on the map
+    public Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
